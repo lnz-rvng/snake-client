680773160d14906cd78b2a440d72d21878d0ce20
Lance <lanceirvingmanzanal@icloud.com>
2024-01-22 21:12:51 -0700

    update the client.js file

--- client.js
+++ client.js
@@ -9,12 +9,62 @@ const connect = () => {
   conn.on('connect', () => {
     console.log(`Successfully connected to game server`);
     conn.write(`Name: LIM`);
-
+    
+    setTimeout(() => {
+      conn.write(`Move: up`);
+      setTimeout(() => {
+        conn.write(`Move: up`);
+        setTimeout(() => {
+          conn.write(`Move: up`);
+          setTimeout(() => {
+            conn.write(`Move: left`);
+            setTimeout(() => {
+              conn.write(`Move: left`);
+              setTimeout(() => {
+                conn.write(`Move: left`);
+                setTimeout(() => {
+                  conn.write(`Move: up`);
+                  setTimeout(() => {
+                    conn.write(`Move: up`);
+                    setTimeout(() => {
+                      conn.write(`Move: up`);
+                      setTimeout(() => {
+                        conn.write(`Move: right`);
+                        setTimeout(() => {
+                          conn.write(`Move: right`);
+                          setTimeout(() => {
+                            conn.write(`Move: right`);
+                            setTimeout(() => {
+                              conn.write(`Move: down`);
+                              setTimeout(() => {
+                                conn.write(`Move: down`);
+                                setTimeout(() => {
+                                  conn.write(`Move: down`);
+                                  setTimeout(() => {
+                                    conn.write(`Move: down`);
+                                    }, 200)
+                                  }, 200)
+                                }, 200)
+                              }, 200)
+                            }, 200)
+                          }, 200)
+                       }, 200)
+                     }, 200)
+                    }, 200)
+                  }, 200)
+                }, 200)
+              }, 200)
+            }, 200)
+          }, 200)
+        }, 200)
+      }, 200)
+ 
     conn.on('data', (data) => {
       console.log(data);
     });
   });
-
+  
+  
   return conn;
 };
 
