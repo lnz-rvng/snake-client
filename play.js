fb7dcb6fa03adf611c9835b4a675c3496226cddf
Lance <lanceirvingmanzanal@icloud.com>
2024-01-22 21:42:06 -0700

    update the play.js file

--- play.js
+++ play.js
@@ -2,3 +2,21 @@ const {connect} = require('./client');
 
 console.log(`Connecting...`);
 connect();
+
+const handleUserInput = () => {
+  if ('\u0003') {
+    process.exit();
+  }
+  
+}
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+setupInput();
