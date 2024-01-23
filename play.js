c52b96815057bb518b3005d24d219ae15af405c1
Lance <lanceirvingmanzanal@icloud.com>
2024-01-22 19:13:27 -0700

    update the play.js file

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require('net');
+const {connect} = require('./client');
 
-const connect = () => {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.setEncoding('utf8');
-  conn.on('connect', () => {
-    console.log(`You connected to the game`);
-
-    conn.on('data', (data) => {
-      console.log(data);
-    });
-  });
-
-  return conn;
-};
-
-console.log(`Connecting ...`);
+console.log(`Connecting...`);
 connect();
