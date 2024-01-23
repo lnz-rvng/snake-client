52355dcf4bb100fe8efbd6f6d06852a14a68dcb4
Lance <lanceirvingmanzanal@icloud.com>
2024-01-22 17:33:12 -0700

    update the play.js file to connect to a game of snake

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log(`You connected to the game`);
+
+    conn.on('data', (data) => {
+      console.log(data);
+    });
+  });
+
+  return conn;
+};
+
+console.log(`Connecting ...`);
+connect();
